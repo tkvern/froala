b2adce097079ecabfe90782390184c486f165c71
tkvern <tkvern@qq.com>
2016-02-23 14:27:04 +0100

    初始化编辑器,图片上传暂无返回值

---
+++ app/controllers/editor_controller.rb
@@ -0,0 +1,17 @@
+class EditorController < ApplicationController
+  def index
+    @uptoken = uptoken
+  end
+
+  def uptoken
+    put_policy = Qiniu::Auth::PutPolicy.new(
+      "playmall",                    # 存储空间
+      nil,                           # 最终资源名，可省略，即缺省为“创建”语义
+      1800,                          # 相对有效期，可省略，缺省为3600秒后 uptoken 过期
+      (Time.now + 30.minutes).to_i  # 绝对有效期，可省略，指明 uptoken 过期期限（绝对值），通常用于调试
+    )
+
+    uptoken = Qiniu::Auth.generate_uptoken(put_policy)
+  end
+
+end
